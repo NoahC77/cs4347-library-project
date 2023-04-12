aab98d6e798e5125ed14af8d047e035d4eac182d
MF2025 <mlf200000@utdallas.edu>
2023-04-11 23:05:06 -0500

    Added hamburger (can always return to the menu from any page) and the login button (can always login)

--- src/frontend/src/components/option.jsx
+++ src/frontend/src/components/option.jsx
@@ -1,16 +1,18 @@
+import { useState, useContext } from 'react'
+import { Context } from '../App'
+
 function Option(props) {
-  const item1 = ""
-  const item2 = "text-right"
+  const { page, setPage } = useContext(Context)
 
   return (
     <>
-      <div className="flex gap-[2vw] mx-[15vw]">
+      <div className="flex gap-[2vw] mx-[15vw]" onClick={props.onClick}>
         <div className="
-          h-[5vh] w-[70vw] bg-[#2288BB] rounded-[5px] mb-[5vh] px-[2vw]
+          h-[5vh] w-[70vw] bg-[#2288BB] rounded-[5px] mb-[5vh] px-[2vw] [cursor:pointer]
           text-white text-[3vh] grid grid-cols-2 gap-[2vw]
         ">
-          <div className={item1}>{props.name}</div>
-          <div className={item2}>{props.stock}</div>
+          <div className={props.className1}>{props.name}</div>
+          <div className={props.className2}>{props.stock}</div>
         </div>
       </div>
     </>
